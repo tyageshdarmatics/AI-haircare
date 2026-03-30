93ca2455761d42aa20d9c11d9b3ad17b6b934711
tyageshdarmatics <tyagesh.darmatics2002@gmail.com>
2026-03-30 15:09:00 +0530

    all network interfaces (0.0.0.0), which is best practice for AWS App Runner

--- server/server.js
+++ server/server.js
@@ -202,6 +202,6 @@ if (fs.existsSync(distPath)) {
   });
 }
 
-app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${port}`);
 });
